9749d9fa1ca53136bbb9c3727553b5a876885bd3
Unknown <lille.bob@hotmail.com>
2018-10-02 11:09:04 +0200

    Enemy bullets and movement

--- Captain-Functional-Amethyst/src/systems/bullet_collision.rs
+++ Captain-Functional-Amethyst/src/systems/bullet_collision.rs
@@ -1,32 +1,35 @@
 use amethyst::core::timing::Time;
 use amethyst::core::transform::{Transform};
-use amethyst::ecs::prelude::{Join, Read, ReadStorage, System, WriteStorage};
+use amethyst::ecs::prelude::{Join, Read, ReadStorage, System, WriteStorage, Write};
 use amethyst::core::cgmath::{Vector2};
-use captain_functional::{Player_Bullet, ARENA_WIDTH, Enemy, BULLET_SIZE, ENEMY_SIZE};
+use captain_functional::{Player_Bullet, ARENA_WIDTH, Enemy, BULLET_SIZE, ENEMY_SIZE, GameStats, ENEMY_SCORE_REWARD};
 
 /// This system is responsible for moving all balls according to their speed
 /// and the time passed.
 pub struct BulletCollision;
 const Bullet_Speed: f32 = 500.0;
 
+
+
 impl<'s> System<'s> for BulletCollision {
     type SystemData = (
         WriteStorage<'s, Player_Bullet>,
 		WriteStorage<'s, Enemy>,
         ReadStorage<'s, Transform>,
-		Read<'s,  Time>,
+		Write<'s, GameStats>,
     );
 
-    fn run(&mut self, (mut bullets, mut enemies,  transforms, time): Self::SystemData) {
+    fn run(&mut self, (mut bullets, mut enemies,  transforms, mut game_stats): Self::SystemData) {
         for (bullet, bullet_trans) in (&mut bullets, &transforms).join() {
 			if bullet.active{
 				for (enemy, enemy_trans) in (&mut enemies, &transforms).join() {
 					if enemy.active{
 						if rect_overlap(bullet_trans.translation[0], bullet_trans.translation[1], BULLET_SIZE, enemy_trans.translation[0], enemy_trans.translation[1], ENEMY_SIZE){
-							println!("HIT!!");
-							println!("x1:{} y1:{}, x2:{} y2{}",bullet_trans.translation[0], bullet_trans.translation[1], enemy_trans.translation[0], enemy_trans.translation[1]);
 							bullet.active = false;
 							enemy.active = false;
+							game_stats.score += ENEMY_SCORE_REWARD;
+
+							println!("Score: {}, Player Health: {}", game_stats.score, game_stats.player_health);
 						}
 					}
 				}
--- Captain-Functional-Amethyst/src/systems/enemy_bullet_movement.rs
+++ Captain-Functional-Amethyst/src/systems/enemy_bullet_movement.rs
@@ -1,34 +1,35 @@
 use amethyst::core::timing::Time;
 use amethyst::core::transform::{Transform};
 use amethyst::ecs::prelude::{Join, Read, ReadStorage, System, WriteStorage};
-use amethyst::core::cgmath::{Vector2};
-use captain_functional::{EnemyBullet, ARENA_WIDTH, Enemy, BULLET_SIZE, ENEMY_SIZE};
+use captain_functional::{EnemyBullet, ARENA_WIDTH};
 
 /// This system is responsible for moving all balls according to their speed
 /// and the time passed.
-pub struct BulletCollision;
-const Bullet_Speed: f32 = 500.0;
+pub struct Enemy_Bullet_Movement;
+const Bullet_Speed: f32 = 700.0;
 
-impl<'s> System<'s> for BulletCollision {
+impl<'s> System<'s> for Enemy_Bullet_Movement {
     type SystemData = (
         WriteStorage<'s, EnemyBullet>,
-        ReadStorage<'s, Transform>,
+        WriteStorage<'s, Transform>,
 		Read<'s,  Time>,
     );
 
-    fn run(&mut self, (mut bullets, mut enemies,  transforms, time): Self::SystemData) {
-        for (bullet, bullet_trans) in (&mut bullets, &transforms).join() {
+    fn run(&mut self, (mut bullets, mut transforms, time): Self::SystemData) {
+        for (bullet, transform) in (&mut bullets, &mut transforms).join() {
+			if bullet.active && bullet.spawn{
+				bullet.spawn = false;
+				transform.translation[0] = bullet.spawn_x;
+				transform.translation[1] = bullet.spawn_y;
+			}
 			if bullet.active{
-				for (enemy, enemy_trans) in (&mut enemies, &transforms).join() {
-					if enemy.active{
-						if rect_overlap(bullet_trans.translation[0], bullet_trans.translation[1], BULLET_SIZE, enemy_trans.translation[0], enemy_trans.translation[1], ENEMY_SIZE){
-							println!("HIT!!");
-							println!("x1:{} y1:{}, x2:{} y2{}",bullet_trans.translation[0], bullet_trans.translation[1], enemy_trans.translation[0], enemy_trans.translation[1]);
-							bullet.active = false;
-							enemy.active = false;
-						}
-					}
+				transform.translation[0] = (transform.translation[0] - Bullet_Speed*time.delta_seconds());
+				if transform.translation[0] < -100.0{
+					bullet.active = false;
 				}
+			}
+			else{
+				transform.translation[0] = ARENA_WIDTH+100.0;
 			}
         }
     }
